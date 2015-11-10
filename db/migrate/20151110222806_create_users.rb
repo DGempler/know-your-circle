9e3ba411e8f57cec264c88e7ba38bb211a7aea7b
Dmitry Gempler <DGempler@gmail.com>
2015-11-10 14:44:14 -0800

    added migration and model for people table

---
+++ db/migrate/20151110222806_create_users.rb
@@ -0,0 +1,17 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.timestamps
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :sex
+      t.string :nickname
+      t.string :bio
+      t.string :location
+      t.string :occupation
+      t.string :image_url
+      t.date :dob
+    end
+  end
+end
