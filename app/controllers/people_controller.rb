c2be0bc88baf8308ec21c8f5446531f25d355650
Dmitry Gempler <DGempler@gmail.com>
2015-11-10 20:01:01 -0800

    finished basic form for submitting name and uploading image (no work on backend yet)

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,16 +7,33 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
 
+  def create
+    @person = Person.new person_params
+
+    if @person.save
+      redirect_to @author
+    else
+      render :new
+    end
+  end
+
   def show
+    @person = Person.find(params[:id])
   end
 
   def edit
   end
 
   def update
+
   end
 
   def destroy
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:first_name, :last_name)
   end
 
 end
