47d267b4e590408af0998f8d84fb87a607457ebc
Dmitry Gempler <DGempler@gmail.com>
2015-11-11 18:39:40 -0800

    added person , /api to /people route, Person and PersonFactory factories. Still need to work out some issues with  config

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -3,7 +3,7 @@ var app = angular.module('memPeeps', ['ngRoute', 'ngResource']);
 app.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
   // $locationProvider.html5Mode(true);
 
-  // $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
 
   $routeProvider
     .when('/', {
--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -5,10 +5,18 @@ angular.module('memPeeps')
   .controller('homeController', ['$scope', function($scope) {
 
   }])
-  .controller('newPersonController', ['$scope', function($scope) {
-    $scope.submitPerson = function() {
+  .controller('newPersonController', ['$scope', 'Person', 'PersonFactory', function($scope, Person, PersonFactory) {
+    $scope.createPerson = function() {
       console.log($scope.person.first_name + " " + $scope.person.last_name);
       console.log($scope.person.image);
+
+      var person = new Person($scope.person);
+      person.$save().then(function(createdPerson) {
+        // PersonFactory.people.push(createdPerson);
+        $scope.person = {};
+      });
+
     };
+
   }]);
 
