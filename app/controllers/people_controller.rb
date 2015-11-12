d997d2e5b1c4fde186da91bec87dfbcdca2eed84
Dmitry Gempler <DGempler@gmail.com>
2015-11-12 08:01:45 -0800

    working on getting image uploaded to rails server from angular, added ng-file-upload module

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,6 +7,10 @@ class PeopleController < ApplicationController
   end
 
   def create
+    if params[:image]
+      decode_image
+    end
+
     @person = Person.new person_params
     if @person.save
       render json: as_json(@person), status: :created
@@ -44,6 +48,25 @@ class PeopleController < ApplicationController
 
   def as_json(options={})
     { first_name: options.first_name, last_name: options.last_name, image: options.image.url(:medium) }
+  end
+
+  def decode_image
+    # decode base64 string
+    Rails.logger.info 'decoding now'
+    decoded_data = Base64.decode64(params[:image]) # json parameter set in directive scope
+    # create 'file' understandable by Paperclip
+    data = StringIO.new(decoded_data)
+    data.class_eval do
+      attr_accessor :content_type, :original_filename
+    end
+
+    # set file properties
+    # data.content_type = params[:image] # json parameter set in directive scope
+    # data.original_filename = params[:image] # json parameter set in directive scope
+
+    # update hash, I had to set @up to persist the hash so I can pass it for saving
+    # since set_params returns a new hash everytime it is called (and must be used to explicitly list which params are allowed otherwise it throws an exception)
+    @person[:image] = data # user Icon is the model attribute that i defined as an attachment using paperclip generator
   end
 
 end
