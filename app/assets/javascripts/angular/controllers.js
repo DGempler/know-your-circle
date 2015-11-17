e23068974c59b98904a3716848dcbfcf37e9cdf8
Dmitry Gempler <DGempler@gmail.com>
2015-11-17 10:26:21 -0800

    fixed date in show & edit controller, and show template

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -116,7 +116,6 @@ angular.module('memPeeps')
   }])
   .controller('personEditController', ['$scope', '$routeParams', '$location', 'PersonFactory', function($scope, $routeParams, $location, PersonFactory) {
     PersonFactory.getPerson($routeParams.id).then(function(person) {
-      var momentDate = moment(person.dob).format();
       $scope.person = {};
       $scope.person.first_name = person.first_name;
       $scope.person.last_name = person.last_name;
@@ -125,7 +124,7 @@ angular.module('memPeeps')
       $scope.person.middle_name = person.middle_name;
       $scope.person.location = person.location;
       $scope.person.occupation = person.occupation;
-      $scope.person.dob = new Date(momentDate);
+      $scope.person.dob = new Date(person.dob);
       $scope.person.bio = person.bio;
       $scope.person.id = person.id;
       $scope.hints = person.hints;
