ea156556694b064fd4108aed689ad09a30b3521a
Dmitry Gempler <DGempler@gmail.com>
2015-11-17 10:48:08 -0800

    created bare-bones seeds file for students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Person.create([
+{first_name: "Austen", last_name: "Rawle"},
+{first_name: "Breanna", last_name: "Gaudioso"},
+{first_name: "Chris", last_name: "Hansen"},
+{first_name: "Clayton", last_name: "Perszyk"},
+{first_name: "Dmitry", last_name: "Gempler"},
+{first_name: "Donald", last_name: "Brunais"},
+{first_name: "Kate", last_name: "Stearns"},
+{first_name: "Lionel", last_name: "Lints"},
+{first_name: "Mary", last_name: "Mott"},
+{first_name: "Rachel", last_name: "Rouston"},
+{first_name: "Teagan", last_name: "Wrest"}
+])
