8414a96ae34f5f941d5758dfe231eeead755b097
Dmitry Gempler <DGempler@gmail.com>
2015-11-18 11:21:15 -0800

    removed anonymous functions on games index controller

--- app/assets/javascripts/angular/games/games.index.controller.js
+++ app/assets/javascripts/angular/games/games.index.controller.js
@@ -1,22 +1,26 @@
 angular.module('memPeeps')
-  .controller('gamesIndexController', ['$scope', '$uibModal', 'PersonFactory', function($scope, $uibModal, PersonFactory) {
-    $scope.people = {};
+  .controller('gamesIndexController', gamesIndexController);
 
-    PersonFactory.getPeople().then(function(people) {
-      $scope.people.people = people;
-    });
+gamesIndexController.$inject = ['$scope', '$uibModal', 'PersonFactory'];
 
-    $scope.open = function () {
+function gamesIndexController() {
+  $scope.people = {};
 
-      var modalInstance = $uibModal.open({
-        animation: true,
-        templateUrl: 'partials/games/_flashcard_modal.html',
-        controller: 'gamesFlashcardsController',
-        resolve: {
-          people: function () {
-            return $scope.people.people;
-          }
+  PersonFactory.getPeople().then(function(people) {
+    $scope.people.people = people;
+  });
+
+  $scope.open = function () {
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'partials/games/_flashcard_modal.html',
+      controller: 'gamesFlashcardsController',
+      resolve: {
+        people: function () {
+          return $scope.people.people;
         }
-      });
-    };
-  }]);
+      }
+    });
+  };
+}
