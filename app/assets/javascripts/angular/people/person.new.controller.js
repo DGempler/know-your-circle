a6d9673d7898c57d61a70033a670cebb1856ffde
Dmitry Gempler <DGempler@gmail.com>
2015-11-18 11:35:17 -0800

    removed anonymous functions from person new and show contorllers

--- app/assets/javascripts/angular/people/person.new.controller.js
+++ app/assets/javascripts/angular/people/person.new.controller.js
@@ -1,43 +1,47 @@
 angular.module('memPeeps')
-  .controller('personNewController', ['$scope', 'PersonFactory', '$location', function($scope, PersonFactory, $location) {
-    $scope.person = {};
-    $scope.hints = {};
-    $scope.hints = [{hint: ""}];
+  .controller('personNewController', personNewController);
 
-    function checkObjectForNullValues(object) {
-      var newObject = {};
-      for (var key in object) {
-        if (object[key] || key == 'image') {
-          newObject[key] = object[key];
-        }
+personNewController.$inject = ['$scope', 'PersonFactory', '$location'];
+
+function personNewController($scope, PersonFactory, $location) {
+  $scope.person = {};
+  $scope.hints = {};
+  $scope.hints = [{hint: ""}];
+
+  function checkObjectForNullValues(object) {
+    var newObject = {};
+    for (var key in object) {
+      if (object[key] || key == 'image') {
+        newObject[key] = object[key];
       }
-      return newObject;
     }
+    return newObject;
+  }
 
-    function checkArrayForNullValues(array) {
-      var newArray = [];
-      array.forEach(function(value) {
-        if (value.hint) {
-          newArray.push(value);
-        }
-      });
-      return newArray;
-    }
-
-    $scope.submitPerson = function() {
-      var newObject = {person: checkObjectForNullValues($scope.person), hints: checkArrayForNullValues($scope.hints)};
-      PersonFactory.createWithAttachment(newObject).then(function(data) {
-        $location.path('/people/show/' + data.id);
-      });
-    };
-
-    $scope.addInputFields = function() {
-      $scope.addedInputFields = true;
-    };
-
-    $scope.addHintInputs = function() {
-      if ($scope.hints.length <= 2) {
-        $scope.hints.push({hint: ''});
+  function checkArrayForNullValues(array) {
+    var newArray = [];
+    array.forEach(function(value) {
+      if (value.hint) {
+        newArray.push(value);
       }
-    };
-  }]);
+    });
+    return newArray;
+  }
+
+  $scope.submitPerson = function() {
+    var newObject = {person: checkObjectForNullValues($scope.person), hints: checkArrayForNullValues($scope.hints)};
+    PersonFactory.createWithAttachment(newObject).then(function(data) {
+      $location.path('/people/show/' + data.id);
+    });
+  };
+
+  $scope.addInputFields = function() {
+    $scope.addedInputFields = true;
+  };
+
+  $scope.addHintInputs = function() {
+    if ($scope.hints.length <= 2) {
+      $scope.hints.push({hint: ''});
+    }
+  };
+}
--- app/assets/javascripts/angular/people/person.show.controller.js
+++ app/assets/javascripts/angular/people/person.show.controller.js
@@ -1,19 +1,23 @@
 angular.module('memPeeps')
-  .controller('personShowController', ['$scope', '$routeParams', '$location', 'PersonFactory', function($scope, $routeParams, $location, PersonFactory) {
-    PersonFactory.getPerson($routeParams.id).then(function(person) {
-      $scope.person = person;
-      if ($scope.person.dob === 'null' || $scope.person.dob === null) {
-        $scope.person.dob = "";
-      } else {
-        $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
+  .controller('personShowController', personShowController);
+
+personShowController.$inject = ['$scope', '$routeParams', '$location', 'PersonFactory'];
+
+function personShowController($scope, $routeParams, $location, PersonFactory) {
+  PersonFactory.getPerson($routeParams.id).then(function(person) {
+    $scope.person = person;
+    if ($scope.person.dob === 'null' || $scope.person.dob === null) {
+      $scope.person.dob = "";
+    } else {
+      $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
+    }
+  });
+
+  $scope.deletePerson = function() {
+    PersonFactory.deletePerson($routeParams.id).then(function(person) {
+      if (person) {
+        $location.path('/people/index');
       }
     });
-
-    $scope.deletePerson = function() {
-      PersonFactory.deletePerson($routeParams.id).then(function(person) {
-        if (person) {
-          $location.path('/people/index');
-        }
-      });
-    };
-  }]);
+  };
+}
