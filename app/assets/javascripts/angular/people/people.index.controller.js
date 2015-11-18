ecea4e1e75fe2ad6b3aeca7f6f2e8b4c36d05ec7
Dmitry Gempler <DGempler@gmail.com>
2015-11-18 11:30:57 -0800

    removed anonymous functions in people index and edit controllers"

--- app/assets/javascripts/angular/people/people.index.controller.js
+++ app/assets/javascripts/angular/people/people.index.controller.js
@@ -1,56 +1,60 @@
 angular.module('memPeeps')
-  .controller('peopleIndexController', ['$scope', 'PersonFactory', '$q', function($scope, PersonFactory, $q) {
-    $scope.people = {};
+  .controller('peopleIndexController', peopleIndexController);
 
-    function getPeople() {
-      PersonFactory.getPeople().then(function(people) {
-        $scope.people.people = people;
+peopleIndexController.$inject = ['$scope', 'PersonFactory', '$q'];
+
+function peopleIndexController($scope, PersonFactory, $q) {
+  $scope.people = {};
+
+  function getPeople() {
+    PersonFactory.getPeople().then(function(people) {
+      $scope.people.people = people;
+    });
+  }
+
+  function deleteSelected() {
+    var promiseArray = [];
+    angular.forEach($scope.people.people, function(person) {
+      if (person.selected) {
+        promiseArray.push(PersonFactory.deletePerson(person.id));
+      }
+    });
+    $q.all(promiseArray).then(function(people) {
+      $scope.people.someoneSelected = false;
+      getPeople();
+    });
+  }
+  $scope.selectAll = function(bool) {
+    if (bool) {
+      $scope.people.someoneSelected = true;
+      angular.forEach($scope.people.people, function(person) {
+        person.selected = true;
+      });
+    } else {
+      $scope.people.someoneSelected = false;
+      angular.forEach($scope.people.people, function(person) {
+        person.selected = false;
       });
     }
+  };
 
-    function deleteSelected() {
-      var promiseArray = [];
-      angular.forEach($scope.people.people, function(person) {
-        if (person.selected) {
-          promiseArray.push(PersonFactory.deletePerson(person.id));
-        }
-      });
-      $q.all(promiseArray).then(function(people) {
-        $scope.people.someoneSelected = false;
-        getPeople();
-      });
+  $scope.personSelected = function() {
+    var selected = false;
+    angular.forEach($scope.people.people, function(person) {
+      if (person.selected) {
+        selected = true;
+      }
+    });
+    $scope.people.someoneSelected = selected;
+  };
+
+  $scope.clickDelete = function() {
+    var confirmDelete = window.confirm("Are you sure?");
+    if (confirmDelete) {
+      deleteSelected();
     }
-    $scope.selectAll = function(bool) {
-      if (bool) {
-        $scope.people.someoneSelected = true;
-        angular.forEach($scope.people.people, function(person) {
-          person.selected = true;
-        });
-      } else {
-        $scope.people.someoneSelected = false;
-        angular.forEach($scope.people.people, function(person) {
-          person.selected = false;
-        });
-      }
-    };
+  };
 
-    $scope.personSelected = function() {
-      var selected = false;
-      angular.forEach($scope.people.people, function(person) {
-        if (person.selected) {
-          selected = true;
-        }
-      });
-      $scope.people.someoneSelected = selected;
-    };
+  getPeople();
 
-    $scope.clickDelete = function() {
-      var confirmDelete = window.confirm("Are you sure?");
-      if (confirmDelete) {
-        deleteSelected();
-      }
-    };
-
-    getPeople();
-
-  }]);
+}
