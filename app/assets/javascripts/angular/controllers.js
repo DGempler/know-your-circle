4bb372adba2e40853eda8ab8ac7b15885f3f28dc
Dmitry Gempler <DGempler@gmail.com>
2015-11-18 07:35:11 -0800

    added check for null dob value before adding to person object, working

--- app/assets/javascripts/angular/controllers.js
+++ app/assets/javascripts/angular/controllers.js
@@ -124,7 +124,9 @@ angular.module('memPeeps')
       $scope.person.middle_name = person.middle_name;
       $scope.person.location = person.location;
       $scope.person.occupation = person.occupation;
-      $scope.person.dob = new Date(person.dob);
+      if (person.dob) {
+        $scope.person.dob = new Date(person.dob);
+      }
       $scope.person.bio = person.bio;
       $scope.person.id = person.id;
       $scope.hints = person.hints;
