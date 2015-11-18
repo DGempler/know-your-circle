4b285d1fd8dafccb5bf05c5442c04a0f9efc3291
Dmitry Gempler <DGempler@gmail.com>
2015-11-18 11:31:09 -0800

    removed anonymous function is person factories

--- app/assets/javascripts/angular/people/Person.factory.js
+++ app/assets/javascripts/angular/people/Person.factory.js
@@ -1,4 +1,8 @@
 angular.module('memPeeps')
-.factory('Person', ['$resource', function($resource) {
+.factory('Person', Person);
+
+Person.$inject = ['$resource'];
+
+function Person($resource) {
   return $resource('//localhost:3000/api/people/:id');
-}]);
+}
