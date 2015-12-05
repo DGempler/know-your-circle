0bc7cf8ac420508c84ebea831ab293cb3f54bfd2
Dmitry Gempler <DGempler@gmail.com>
2015-12-05 08:24:07 -0800

    fixed case where hints are submitted as empty array and weren't clearing out

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -48,6 +48,7 @@ class PeopleController < ApplicationController
 
   private
   def person_params
+    params[:person][:hints] ||= []
     params.require(:person).permit(:first_name, :middle_name, :last_name, :image, :sex, :nickname, :bio, :location, :occupation, :dob, hints: [])
   end
 
