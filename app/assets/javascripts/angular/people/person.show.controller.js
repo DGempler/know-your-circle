021ba18b8ac135cc2cc82132c1d2c211ec7f7cba
Dmitry Gempler <DGempler@gmail.com>
2015-12-05 06:31:21 -0800

    fixed hints for show controller

--- app/assets/javascripts/angular/people/person.show.controller.js
+++ app/assets/javascripts/angular/people/person.show.controller.js
@@ -12,6 +12,13 @@
       } else {
         $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
       }
+      var tempArray = [];
+      $scope.person.hints.forEach(function(hint) {
+        if (hint) {
+          tempArray.push(hint);
+        }
+      });
+      $scope.person.hints = tempArray;
     });
 
     $scope.deletePerson = function() {
