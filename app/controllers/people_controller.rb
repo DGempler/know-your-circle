a4dc1117b1e5e80759d9a4afa92d8313b165f676
Dmitry Gempler <DGempler@gmail.com>
2015-12-04 16:17:34 -0800

    modified people controller for new hint property, removed old hint params and associations

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,7 +7,7 @@ class PeopleController < ApplicationController
     @people.each do |person|
       medium = person.image.url(:medium)
       thumb = person.image.url(:thumb)
-      person = person.as_json(:include => :hints)
+      person = person.as_json
       person[:medium] = medium
       person[:thumb] = thumb
       people << person
@@ -18,15 +18,7 @@ class PeopleController < ApplicationController
   def create
     @person = Person.new person_params
     if @person.save
-      hints_params['hints'].each do |index, hint|
-        new_hint = Hint.create hint
-        @person.hints << new_hint
-      end
-      if @person.save
-        render json: @person, status: :created
-      else
-        render json: @person.errors, status: :unprocessable_entity
-      end
+      render json: @person, status: :created
     else
       render json: @person.errors, status: :unprocessable_entity
     end
@@ -35,7 +27,7 @@ class PeopleController < ApplicationController
   def show
     medium = @person.image.url(:medium)
     thumb = @person.image.url(:thumb)
-    @person = @person.as_json(:include => :hints)
+    @person = @person.as_json
     @person[:medium] = medium
     @person[:thumb] = thumb
     render json: @person, status: :ok
@@ -43,20 +35,6 @@ class PeopleController < ApplicationController
 
   def update
     if @person.update person_params
-      puts 'done'
-      hints_params['hints'].each do |index, hint|
-        if hint[:hint] === ""
-          Hint.find(hint[:id]).destroy
-        else
-          begin
-            Hint.find(hint[:id]).update Hash[:hint,  hint[:hint]]
-          rescue ActiveRecord::RecordNotFound
-            new_hint = Hint.create Hash[:hint,  hint[:hint]]
-            @person.hints << new_hint
-            @person.save
-          end
-        end
-      end
       render json: @person, status: :ok
     else
       render json: @person.errors, status: :unprocessable_entity
@@ -70,12 +48,7 @@ class PeopleController < ApplicationController
 
   private
   def person_params
-    params.require(:person).permit(:first_name, :middle_name, :last_name, :image, :sex, :nickname, :bio, :location, :occupation, :dob)
-  end
-
-  def hints_params
-    params[:hints] ||= []
-    params.permit(hints: [:'0', :"1", :"2", :id, :hint])
+    params.require(:person).permit(:first_name, :middle_name, :last_name, :image, :sex, :nickname, :bio, :location, :occupation, :dob, hints: [])
   end
 
   def set_person
