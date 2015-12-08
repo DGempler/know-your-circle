07602a0cb41145a24a89c6e5714778efd51e206d
Dmitry Gempler <DGempler@gmail.com>
2015-12-08 14:30:18 -0800

    added mailcatcher configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = "/usr/local/bin"
 
+  # Mailcatcher configuration
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+
 end
