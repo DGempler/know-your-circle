79768337d41ab7f32b2d01a900557ad8d9441ea9
Dmitry Gempler <DGempler@gmail.com>
2015-12-08 15:59:38 -0800

    fixed default url in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin"
 
   # Mailcatcher configuration
-  config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
 
