e43aecf9e1045b7f4b0a434010f8155cdf360ee6
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 13:41:57 -0800

    added override for token success validation on token_validations controller

--- app/controllers/overrides/token_validations_controller.rb
+++ app/controllers/overrides/token_validations_controller.rb
@@ -1,32 +1,39 @@
-module Overrides
-  class TokenValidationsController < DeviseTokenAuth::TokenValidationsController
+module DeviseTokenAuth
+  class TokenValidationsController < DeviseTokenAuth::ApplicationController
     skip_before_filter :assert_is_devise_resource!, :only => [:validate_token]
     before_filter :set_user_by_token, :only => [:validate_token]
 
     def validate_token
-      # @resource will have been set by set_user_by_token concern
-      if @user
-        medium = @user.image.url(:medium)
-        puts "medium"
-        puts medium
-        thumb = @user.image.url(:thumb)
-        puts "thumb"
-        puts thumb
-        @user = @user.as_json(except: [
-          :tokens, :created_at, :updated_at
-        ])
-        @user[:medium] = medium
-        @user[:thumb] = thumb
-        render json: {
-          success: true,
-          data: @user
-        }
+      # @resource will have been set by set_user_token concern
+      if @resource
+        yield if block_given?
+        render_validate_token_success
       else
-        render json: {
-          success: false,
-          errors: ["Invalid login credentials"]
-        }, status: 401
+        render_validate_token_error
       end
+    end
+
+    protected
+
+    def render_validate_token_success
+      medium = @resource.image.url(:medium)
+      thumb = @resource.image.url(:thumb)
+      @resource_json = @resource.as_json(except: [
+        :tokens, :created_at, :updated_at
+      ])
+      @resource_json[:medium] = medium
+      @resource_json[:thumb] = thumb
+      render json: {
+        success: true,
+        data:   @resource_json
+      }
+    end
+
+    def render_validate_token_error
+      render json: {
+        success: false,
+        errors: [I18n.t("devise_token_auth.token_validations.invalid")]
+      }, status: 401
     end
   end
 end
