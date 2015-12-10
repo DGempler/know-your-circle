dbf873bd4ab764286b64b5b3b3180b32430b7e6c
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 07:55:08 -0800

    adding user update profile functionality

--- app/assets/javascripts/angular/users/users.profile.controller.js
+++ app/assets/javascripts/angular/users/users.profile.controller.js
@@ -2,12 +2,21 @@
   angular.module('memPeeps.users')
     .controller('profileController', profileController);
 
-  profileController.$inject=['$rootScope'];
+  profileController.$inject=['$rootScope', '$auth'];
 
-  function profileController($rootScope) {
+  function profileController($rootScope, $auth) {
     var vm = this;
     vm.user = $rootScope.user;
 
+    vm.submitUserUpdateAccount = function() {
+      $auth.updateAccount(vm.user)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
   }
 
 })();
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+    devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :middle_name, :sex, :nickname, :bio, :location, :occupation, :dob, :image]
   end
 
   include DeviseTokenAuth::Concerns::SetUserByToken
