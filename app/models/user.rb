24decff872b4e7a272182a2898bddcd65a5e1367
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 08:34:22 -0800

    added image to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :first_name, :last_name
   validates_uniqueness_of :email
 
+  # This method associates the attribute ":image_url" with a file attachment
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
