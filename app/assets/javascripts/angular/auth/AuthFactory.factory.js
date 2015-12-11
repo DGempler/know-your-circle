b65d6c6d0860908f55e39f636c47abf7c9f2c120
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 18:43:00 -0800

    moved login and signup open, and logout functions into AuthFactory

--- app/assets/javascripts/angular/auth/AuthFactory.factory.js
+++ app/assets/javascripts/angular/auth/AuthFactory.factory.js
@@ -2,10 +2,42 @@
   angular.module('memPeeps.auth')
     .factory('AuthFactory', AuthFactory);
 
-    AuthFactory.$inject = ['$q'];
+    AuthFactory.$inject = ['$uibModal', '$auth', '$location'];
 
-    function AuthFactory($q) {
+    function AuthFactory($uibModal, $auth, $location) {
       var factory = {};
+
+      factory.logInOpen = function() {
+        var modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: '/partials/auth/_login_modal.html',
+          controller: 'loginController as auth',
+          size: 'sm',
+          windowClass: "modal fade"
+        });
+      };
+
+      factory.signUpOpen = function() {
+        var modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: '/partials/auth/_signup_modal.html',
+          controller: 'signupController as auth',
+          size: 'sm',
+          windowClass: "modal fade"
+        });
+      };
+
+      factory.logOut = function() {
+        $auth.signOut()
+          .then(function(resp) {
+            console.log('logged out!');
+            $location.path('/');
+          })
+          .catch(function(error) {
+            console.log(error);
+            $location.path('/');
+          });
+      };
 
       return factory;
     }
--- app/assets/javascripts/angular/auth/auth.nav.controller.js
+++ app/assets/javascripts/angular/auth/auth.nav.controller.js
@@ -8,45 +8,15 @@
     var vm = this;
 
     vm.logInOpen = function() {
-
-      var modalInstance = $uibModal.open({
-        animation: true,
-        templateUrl: '/partials/auth/_login_modal.html',
-        controller: 'loginController as auth',
-        size: 'sm',
-        windowClass: "modal fade"
-      });
-
+      AuthFactory.logInOpen();
     };
 
     vm.signUpOpen = function() {
-
-      var modalInstance = $uibModal.open({
-        animation: true,
-        templateUrl: '/partials/auth/_signup_modal.html',
-        controller: 'signupController as auth',
-        size: 'sm',
-        windowClass: "modal fade"
-      });
+      AuthFactory.signUpOpen();
     };
 
     vm.logOut = function() {
-      $auth.signOut()
-        .then(function(resp) {
-          $location.path('/');
-        })
-        .catch(function(error) {
-          console.log(error);
-          $location.path('/');
-        });
-    };
-
-    AuthFactory.logInOpen = function() {
-      vm.logInOpen();
-    };
-
-    AuthFactory.signUpOpen = function() {
-      vm.signUpOpen();
+      AuthFactory.logOut();
     };
 
   }
