bb3753d18bdb55d53b43bd9d9cfa6099983b3a4c
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 16:51:23 -0800

    removed commented code

--- app/assets/javascripts/angular/users/users.changePassword.controller.js
+++ app/assets/javascripts/angular/users/users.changePassword.controller.js
@@ -2,20 +2,10 @@
   angular.module('memPeeps.users')
     .controller('changePasswordController', changePasswordController);
 
-    changePasswordController.$inject = ['$uibModalInstance', '$auth', '$rootScope'];
+    changePasswordController.$inject = ['$uibModalInstance', '$auth', '$rootScope', '$location'];
 
-    function changePasswordController($uibModalInstance, $auth, $rootScope) {
+    function changePasswordController($uibModalInstance, $auth, $rootScope, $location) {
       var vm = this;
-
-      // vm.user = {};
-
-      // function copyUser() {
-      //   for (var prop in $rootScope.user) {
-      //     vm.user[prop] = $rootScope.user[prop];
-      //   }
-      // }
-
-      // copyUser();
 
       vm.closeModal = function() {
         $uibModalInstance.close();
