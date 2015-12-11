bb206e8f277319c6d444200170474d38dcc2a903
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 16:47:49 -0800

    added submitChangePassword function

--- app/assets/javascripts/angular/users/users.changePassword.controller.js
+++ app/assets/javascripts/angular/users/users.changePassword.controller.js
@@ -2,12 +2,36 @@
   angular.module('memPeeps.users')
     .controller('changePasswordController', changePasswordController);
 
-    changePasswordController.$inject = [];
+    changePasswordController.$inject = ['$uibModalInstance', '$auth', '$rootScope'];
 
-    function changePasswordController() {
+    function changePasswordController($uibModalInstance, $auth, $rootScope) {
+      var vm = this;
 
+      // vm.user = {};
 
+      // function copyUser() {
+      //   for (var prop in $rootScope.user) {
+      //     vm.user[prop] = $rootScope.user[prop];
+      //   }
+      // }
 
+      // copyUser();
+
+      vm.closeModal = function() {
+        $uibModalInstance.close();
+      };
+
+      vm.submitChangePassword = function() {
+        $auth.updateAccount(vm.user)
+          .then(function(res) {
+            $uibModalInstance.close();
+            $location.path('/profile');
+          })
+          .catch(function(err) {
+            $uibModalInstance.close();
+            console.log(err);
+          });
+      };
 
     }
 
