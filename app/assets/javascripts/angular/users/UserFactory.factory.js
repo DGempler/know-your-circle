5f10a693f8a12e6ad5de55c342f01343d9181e40
Dmitry Gempler <DGempler@gmail.com>
2015-12-10 16:27:49 -0800

    created empty UserFactory

---
+++ app/assets/javascripts/angular/users/UserFactory.factory.js
@@ -0,0 +1,14 @@
+(function() {
+  angular.module('memPeeps.users')
+    .factory('UserFactory', UserFactory);
+
+    UserFactory.$inject = [];
+
+    function UserFactory() {
+
+
+
+
+    }
+
+})();
