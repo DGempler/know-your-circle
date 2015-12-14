c860f361d80f709486e89493d694f40deb70e052
Dmitry Gempler <DGempler@gmail.com>
2015-12-14 10:57:17 -0800

    added default avatar for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ActiveRecord::Base
   has_attached_file :image, styles: {
     thumb: '100x100>',
     medium: '300x300>'
-  }
+  },
+  default_url: "/assets/images/:style/missing.png"
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
