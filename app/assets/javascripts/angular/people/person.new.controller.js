44b7db7428bc31b2fa5537ba22fcab9e171ab284
Dmitry Gempler <DGempler@gmail.com>
2015-12-15 15:53:58 -0800

    setting up group select on new person controller and html

--- app/assets/javascripts/angular/people/person.new.controller.js
+++ app/assets/javascripts/angular/people/person.new.controller.js
@@ -8,6 +8,14 @@
     var vm = this;
     vm.person = {};
     vm.person.hints = [""];
+    vm.person.groups = ['friends', 'enemies'];
+    vm.person.groups.push('Create a new group');
+
+    vm.getGroup = function() {
+      if (vm.person.group === 'Create a new group') {
+        console.log('open sesame!');
+      }
+    };
 
     // prevent "null" from being sent as a value to server if form field is left blank
     function removeNullValues(submittedPerson, newPerson) {
