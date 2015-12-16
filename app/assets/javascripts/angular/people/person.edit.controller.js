0f335fdbf3db8985d29fe7295e260487e30fdce0
Dmitry Gempler <DGempler@gmail.com>
2015-12-16 10:33:11 -0800

    added labels to person edit html and controller

--- app/assets/javascripts/angular/people/person.edit.controller.js
+++ app/assets/javascripts/angular/people/person.edit.controller.js
@@ -2,36 +2,58 @@
   angular.module('memPeeps.people')
     .controller('personEditController', personEditController);
 
-  personEditController.$inject = ['$routeParams', '$location', 'PersonFactory'];
+  personEditController.$inject = ['$routeParams', '$location', 'PersonFactory', 'GroupFactory'];
 
-  function personEditController($routeParams, $location, PersonFactory) {
+  function personEditController($routeParams, $location, PersonFactory, GroupFactory) {
     var vm = this;
     var originalPerson;
-    PersonFactory.getPerson($routeParams.id).then(function(person) {
-      originalPerson = person;
-      vm.person = {};
-      vm.person.first_name = person.first_name;
-      vm.person.last_name = person.last_name;
-      vm.person.sex = person.sex;
-      vm.person.nickname = person.nickname;
-      vm.person.middle_name = person.middle_name;
-      vm.person.location = person.location;
-      vm.person.occupation = person.occupation;
-      if (person.dob) {
-        vm.person.dob = new Date(person.dob);
+    var originalGroups;
+
+    function getGroups() {
+      GroupFactory.getGroups()
+        .then(function(groups) {
+          originalGroups = groups;
+          vm.groups = [];
+          groups.forEach(function(group) {
+            vm.groups.push(group);
+          });
+          vm.groups.push({name: 'Create a new group'});
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+
+    vm.groupSelected = function() {
+      if (vm.chosenGroup.name === 'Create a new group') {
+        vm.chosenGroup = "";
+        GroupFactory.openNewGroupModal(originalGroups)
+          .then(function(groups) {
+            originalGroups = groups;
+            vm.groups = [];
+            groups.forEach(function(group) {
+              vm.groups.push(group);
+            });
+            vm.groups.push({name: 'Create a new group'});
+          })
+          .catch(function() {
+            getGroups();
+          });
+      } else {
+        if (vm.person.group_ids.indexOf(vm.chosenGroup.id) === -1) {
+          vm.person.group_ids.push(vm.chosenGroup.id);
+        }
+        vm.chosenGroup = "";
       }
-      vm.person.bio = person.bio;
-      vm.person.id = person.id;
-      vm.person.hints = [];
-      person.hints.forEach(function(hint) {
-        if (hint) {
-          vm.person.hints.push(hint);
+    };
+
+    vm.removeGroup = function(removeId) {
+      vm.person.group_ids.forEach(function(groupId, index) {
+        if (removeId === groupId) {
+          vm.person.group_ids.splice(index, 1);
         }
       });
-      if (vm.person.hints.length === 0) {
-        vm.person.hints.push("");
-      }
-    });
+    };
 
     // prevent "null" from being sent as a value to server if form field is left blank
     function removeNullValues(submittedPerson, newPerson) {
@@ -76,5 +98,37 @@
         vm.person.hints.push('');
       }
     };
+
+    PersonFactory.getPerson($routeParams.id).then(function(person) {
+      originalPerson = person;
+      vm.person = {};
+      vm.person.first_name = person.first_name;
+      vm.person.last_name = person.last_name;
+      vm.person.sex = person.sex;
+      vm.person.nickname = person.nickname;
+      vm.person.middle_name = person.middle_name;
+      vm.person.location = person.location;
+      vm.person.occupation = person.occupation;
+      if (person.dob) {
+        vm.person.dob = new Date(person.dob);
+      }
+      vm.person.bio = person.bio;
+      vm.person.id = person.id;
+      vm.person.group_ids = [];
+      person.groups.forEach(function(group) {
+        vm.person.group_ids.push(group.id);
+      });
+      vm.person.hints = [];
+      person.hints.forEach(function(hint) {
+        if (hint) {
+          vm.person.hints.push(hint);
+        }
+      });
+      if (vm.person.hints.length === 0) {
+        vm.person.hints.push("");
+      }
+    });
+
+    getGroups();
   }
 })();
