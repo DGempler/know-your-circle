fe97ea713a756ba80b5143d5c68406bdee27d1a5
Dmitry Gempler <DGempler@gmail.com>
2015-12-15 18:33:05 -0800

    added group validation and uniqueness (scoped to each user)

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :memberships, dependent: :destroy
   has_many :people, through: :memberships
+  validates :name, uniqueness: { scope: :user_id }
 end
