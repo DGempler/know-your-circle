8e7d152591d4ce09f1c63a659aeacc0128db971c
Dmitry Gempler <DGempler@gmail.com>
2015-12-15 16:00:35 -0800

    created empty GroupFactory

---
+++ app/assets/javascripts/angular/groups/GroupFactory.factory.js
@@ -0,0 +1,12 @@
+(function() {
+  angular.module('memPeeps.groups')
+    .factory('GroupFactory', GroupFactory);
+
+    GroupFactory.$inject = [];
+
+    function GroupFactory() {
+      var factory = {};
+
+      return factory;
+    }
+})();
