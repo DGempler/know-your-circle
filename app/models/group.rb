7baf7da3823f6725ff807ae183ba063d468dc6ab
Dmitry Gempler <DGempler@gmail.com>
2015-12-17 15:15:06 -0800

    added case insensitive uniqueness validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,6 @@ class Group < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :people, through: :memberships
   validates :name, uniqueness: { scope: :user_id ,
+    case_sensitive: false,
     message: "This group already exists" }
 end
