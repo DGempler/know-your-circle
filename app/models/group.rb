1bac893b2196bdaef9150dff9e421e2ad7c6756e
Dmitry Gempler <DGempler@gmail.com>
2015-12-17 15:05:55 -0800

    added custom error message for name uniqueness check

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :memberships, dependent: :destroy
   has_many :people, through: :memberships
-  validates :name, uniqueness: { scope: :user_id }
+  validates :name, uniqueness: { scope: :user_id ,
+    message: "This group already exists" }
 end
