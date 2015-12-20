1852d95afe873670a1e04cab386824850135a82d
Dmitry Gempler <DGempler@gmail.com>
2015-12-20 00:10:05 -0800

    added  in array syntax for resolves in app.config.js

--- app/assets/javascripts/angular/app.config.js
+++ app/assets/javascripts/angular/app.config.js
@@ -14,63 +14,63 @@
         templateUrl: 'partials/people/index.html',
         controller: 'peopleIndexController as people',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/people/new', {
         templateUrl: 'partials/people/new.html',
         controller: 'personNewController as person',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/people/show/:id', {
         templateUrl: 'partials/people/show.html',
         controller: 'personShowController as person',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/people/:id/edit', {
         templateUrl: 'partials/people/edit.html',
         controller: 'personEditController as person',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/games/index', {
         templateUrl: 'partials/games/index.html',
         controller: 'gamesIndexController as games',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/profile', {
         templateUrl: 'partials/users/profile.html',
         controller: 'profileController as profile',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .when('/profile/edit', {
         templateUrl: 'partials/users/edit.profile.html',
         controller: 'editProfileController as profile',
         resolve: {
-          auth: function($auth) {
+          auth: ['$auth', function($auth) {
             return $auth.validateUser();
-          }
+          }]
         }
       })
       .otherwise({
