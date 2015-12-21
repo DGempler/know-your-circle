795d598e075e65b2214d904001676ae72f3f79ff
Dmitry Gempler <DGempler@gmail.com>
2015-12-21 11:16:43 -0800

    added error message modal for person get and delete, and confirm message modal for delete

--- app/assets/javascripts/angular/people/person.show.controller.js
+++ app/assets/javascripts/angular/people/person.show.controller.js
@@ -2,24 +2,38 @@
   angular.module('memPeeps.people')
     .controller('personShowController', personShowController);
 
-  personShowController.$inject = ['$scope', '$routeParams', '$location', 'PersonFactory'];
+  personShowController.$inject = ['$scope', '$routeParams', '$location', 'PersonFactory', 'UserFactory', 'AuthFactory'];
 
-  function personShowController($scope, $routeParams, $location, PersonFactory) {
-    PersonFactory.getPerson($routeParams.id).then(function(person) {
-      $scope.person = person;
-      if ($scope.person.dob === 'null' || $scope.person.dob === null) {
-        $scope.person.dob = "";
-      } else {
-        $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
-      }
-    });
+  function personShowController($scope, $routeParams, $location, PersonFactory, UserFactory, AuthFactory) {
+      PersonFactory.getPerson($routeParams.id)
+        .then(function(person) {
+          $scope.person = person;
+          if ($scope.person.dob === 'null' || $scope.person.dob === null) {
+            $scope.person.dob = "";
+          } else {
+            $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
+          }
+        })
+        .catch(function(error) {
+          var message = 'An error occured while loading your person. Please refresh the page to try again.';
+          AuthFactory.messageModalOpen(message);
+        });
 
     $scope.deletePerson = function() {
-      PersonFactory.deletePerson($routeParams.id).then(function(person) {
-        if (person) {
-          $location.path('/people/index');
-        }
-      });
+      var message = "Are you sure?";
+      UserFactory.confirmMessageModalOpen(message)
+        .then(function() {
+          PersonFactory.deletePerson($routeParams.id)
+            .then(function(person) {
+              if (person) {
+                $location.path('/people/index');
+              }
+            })
+            .catch(function(error) {
+              var message = 'An error occured while deleting your person. Please refresh the page and try again.';
+              AuthFactory.messageModalOpen(message);
+            });
+        });
     };
   }
 })();
