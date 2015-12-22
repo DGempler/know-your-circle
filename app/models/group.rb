125fd2974b8374f7abf5107e99075c51c70015b0
Dmitry Gempler <DGempler@gmail.com>
2015-12-22 09:11:12 -0800

    added presence: true to group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,9 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :memberships, dependent: :destroy
   has_many :people, through: :memberships
-  validates :name, uniqueness: { scope: :user_id ,
+  validates :name,
+    presence: true,
+    uniqueness: { scope: :user_id ,
     case_sensitive: false,
     message: "This group already exists." }
 end
