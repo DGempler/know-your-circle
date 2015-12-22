0cf48b8e463bc90b00390bc12161df441bfd704d
Dmitry Gempler <DGempler@gmail.com>
2015-12-22 13:42:44 -0800

    added signed in user test for index

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe PeopleController, :type => :controller do
+describe PeopleController, :type => :controller do
   describe "anonymous user" do
     before :each do
       # This simulates an anonymous user
       login_with nil
     end
 
-    it "should be redirected to signin" do
+    it "should get unauthorized 401 status code" do
       get :index
       expect( response.status ).to eq(401)
+    end
+
+  end
+
+  describe "signed in user" do
+    before :each do
+      login_with create( :user )
+    end
+
+    it "should send the user json with all people" do
+      get :index
+      puts response.body
+      expect( JSON.parse(response.body) ).to eq([])
     end
   end
 end
