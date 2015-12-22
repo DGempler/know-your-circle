4feb1b9b36dc5222c21afc7e81c3e47ed92b498f
Dmitry Gempler <DGempler@gmail.com>
2015-12-22 09:12:24 -0800

    removed duplicate test for group name

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -7,7 +7,6 @@ end
 describe Group, 'validations' do
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of(:name).with_message("This group already exists.") }
-  it { should validate_presence_of(:name) }
 end
 
 describe Group, 'associations' do
