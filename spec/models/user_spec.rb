bed0552b284de66155b9c6527a17833a948bc671
Dmitry Gempler <DGempler@gmail.com>
2015-12-22 07:57:14 -0800

    added some shoulda to tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User do
+describe User, 'response' do
   it { is_expected.to respond_to :email }
   it { is_expected.to respond_to :password }
   it { is_expected.to respond_to :first_name }
@@ -13,28 +13,30 @@ describe User do
   it { is_expected.to respond_to :dob }
   it { is_expected.to respond_to :occupation }
   it { is_expected.to respond_to :image }
+end
 
-  describe 'email when it is blank' do
-    subject(:invalid_email) { User.create(email: '', password: 'abcdefgh', first_name: 'John', last_name: 'Doe')}
-    it { is_expected.to_not be_valid }
-  end
+describe User, 'validations' do
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:email) }
 
   describe 'email when it is missing @ symbol' do
     subject(:invalid_email) { User.create(email: 'haha.com', password: 'abcdefgh', first_name: 'John', last_name: 'Doe')}
     it { is_expected.to_not be_valid }
   end
 
-  describe 'email when it has the @ symbol' do
-    subject(:valid_emaik) { User.create(email: 'a@a.com', password: 'abcdefgh', first_name: 'John', last_name: 'Doe')}
+  it { should validate_presence_of(:password) }
+
+  describe 'password when it is less than 8 characters' do
+    subject(:invalid_password) { User.create(email: 'a@a.com', password: 'abcdefg', first_name: 'John', last_name: 'Doe')}
+    it { is_expected.to_not be_valid }
+  end
+  describe 'password when it is 8 characters or longer' do
+    subject(:invalid_password) { User.create(email: 'a@a.com', password: 'abcdefgh', first_name: 'John', last_name: 'Doe')}
     it { is_expected.to be_valid }
   end
 
-  describe '#first_name' do
-    it 'cannot be blank' do
-      invalid_first_name = User.create(email: 'haha.com', password: 'abcdefgh', first_name: '', last_name: 'Doe')
-      expect(invalid_first_name).to_not be_valid
-    end
-  end
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+
 
 end
-
