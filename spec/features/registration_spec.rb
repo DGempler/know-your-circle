90ee1209a5bb4501fb71ddb5dad984a5d8346659
Dmitry Gempler <DGempler@gmail.com>
2015-12-27 01:07:34 -0800

    created valid input registration spec

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "Registration", js: true do
+  before do
+    visit '/'
+    click_link('sign-up')
+  end
+  scenario "with valid inputs" do
+
+    @user = build(:user)
+    fill_in "First Name", with: @user.first_name
+    fill_in "Last Name", with: @user.last_name
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    fill_in "Confirm Password", with: @user.password_confirmation
+    find("button", text: "Sign Up").click
+
+    expect(page).to have_content("A confirmation email has been sent to #{@user.email}")
+  end
+end
