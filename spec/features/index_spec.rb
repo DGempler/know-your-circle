d1de84f6eb9974950c5a5ac482c91191a4132b64
Dmitry Gempler <DGempler@gmail.com>
2015-12-26 23:09:17 -0800

    added anonymous user test for index

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -7,4 +7,15 @@ feature "Visit website", js: true do
     expect(page).to have_content("Memorize names and faces")
     expect(page).to have_content("Welcome to Know your Circle!")
   end
+
+  scenario "anonymous user" do
+    visit '/'
+
+    expect(page).not_to have_content("Get started by adding some people!")
+    expect(page).not_to have_content("Add People")
+    expect(page).not_to have_content("Play Games")
+    expect(page).not_to have_content("Account")
+    expect(page).not_to have_content("My Profile")
+    expect(page).not_to have_content("Log Out")
+  end
 end
