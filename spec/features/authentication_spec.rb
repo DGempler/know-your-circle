a71a6c364a32c7561d42822a3fe98698537d59f3
Dmitry Gempler <DGempler@gmail.com>
2015-12-27 00:25:54 -0800

    added invalid credentials spec

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper.rb'
 
 feature 'Authentication', js: true do
+  before do
+    @user = FactoryGirl.create(:confirmed_user)
+    visit '/'
+    click_link('log-in')
+    @login_modal = LoginModal.new
+  end
+
   feature 'login' do
     scenario 'with valid inputs' do
-      @user = FactoryGirl.create(:confirmed_user)
-      visit '/'
-      click_link('log-in')
-
-      fill_in "Email", with: @user.email
-      fill_in "Password", with: @user.password
-      find("button", text: "Log In").click
-
+      @login_modal.log_in(@user.email, @user.password)
       expect(page).to have_content("Add People")
       expect(page).to have_content("Play Games")
       expect(page).to have_content("Account")
     end
+
+    scenario 'with invalid credentials' do
+      @login_modal.log_in('fake_email@fake.com', 'not a real password')
+      expect(page).to have_content('Please try again')
+    end
+
   end
 end
