90dda640b0efd84b92d0d56cc69323e4e8429a8c
Dmitry Gempler <DGempler@gmail.com>
2015-12-28 09:40:18 -0800

    added visit method to login_modal helper

--- spec/support/login_modal.rb
+++ spec/support/login_modal.rb
@@ -1,6 +1,11 @@
 class LoginModal < Struct.new(:title)
   include Capybara::DSL
 
+  def visit
+    Capybara::visit '/'
+    click_link('log-in')
+  end
+
   def log_in(email, password)
     fill_in "Email", with: email
     fill_in "Password", with: password
