2a4491e0ad12d79b57a585b0b3f599ca9cc4d421
Dmitry Gempler <DGempler@gmail.com>
2015-12-28 09:02:05 -0800

    added short password spec

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -4,10 +4,11 @@ feature "Registration", js: true do
   before do
     visit '/'
     click_link('sign-up')
+    @user = build(:user)
   end
+
   scenario "with valid inputs" do
 
-    @user = build(:user)
     fill_in "First Name", with: @user.first_name
     fill_in "Last Name", with: @user.last_name
     fill_in "Email", with: @user.email
@@ -22,4 +23,17 @@ feature "Registration", js: true do
     expect(ActionMailer::Base.deliveries.first.to).to eq([@user.email])
 
   end
+
+  scenario 'with a too-short password' do
+    password = 'a'
+    fill_in "First Name", with: @user.first_name
+    fill_in "Last Name", with: @user.last_name
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: password
+    fill_in "Confirm Password", with: @user.password_confirmation
+    find("button", text: "Sign Up").click
+
+    expect(page).to have_content('Your password must be at least 8 characters. Please try again.')
+  end
+
 end
