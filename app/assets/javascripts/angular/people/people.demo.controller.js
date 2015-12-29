b279a88d97f711479bfc6ed190a58a3cdee842c4
Dmitry Gempler <DGempler@gmail.com>
2015-12-29 14:29:22 -0800

    removed deleteAll and applyGroup functions

--- app/assets/javascripts/angular/people/people.demo.controller.js
+++ app/assets/javascripts/angular/people/people.demo.controller.js
@@ -29,25 +29,6 @@
         });
     }
 
-    function deleteSelected() {
-      var promiseArray = [];
-      angular.forEach(vm.people, function(person) {
-        if (person.selected) {
-          promiseArray.push(PersonFactory.deletePerson(person.id));
-        }
-      });
-      $q.all(promiseArray)
-        .then(function(people) {
-          vm.someoneSelected = false;
-          getPeople();
-        })
-        .catch(function(errors) {
-          var message = 'An error occured while deleting your people. Please refresh the page and try again.';
-          AuthFactory.messageModalOpen(message);
-        });
-    }
-
-
     vm.selectAllCheckbox = function(bool) {
       if (bool && vm.people.length > 0) {
         vm.selectAll();
@@ -114,59 +95,6 @@
         .then(function() {
           deleteSelected();
         });
-    };
-
-    vm.applyGroup = function(newGroup) {
-      var promiseArray = [];
-      var alreadyExistsArray = [];
-      var message;
-      vm.someoneSelected = false;
-      angular.forEach(vm.people, function(person) {
-        if (person.selected) {
-          person.group_ids = [];
-          person.groups.forEach(function(existingGroup) {
-            person.group_ids.push(existingGroup.id);
-          });
-          if (person.group_ids.indexOf(newGroup.id) === -1) {
-            person.group_ids.push(newGroup.id);
-            promiseArray.push(PersonFactory.updateGroups(person));
-          } else {
-            alreadyExistsArray.push(person);
-          }
-        }
-      });
-      if (promiseArray.length > 0) {
-        $q.all(promiseArray)
-          .then(function(people) {
-            message = newGroup.name + " has been added to: ";
-            people.forEach(function(person, index) {
-              if (index !== people.length - 1) {
-                message += person.first_name + " " + person.last_name + ", ";
-              } else {
-                message += person.first_name + " " + person.last_name + ".";
-              }
-            });
-            if (alreadyExistsArray.length > 0) {
-              message += " It already existed on: ";
-              alreadyExistsArray.forEach(function(person, index) {
-                if (index !== alreadyExistsArray.length - 1) {
-                  message += person.first_name + " " + person.last_name + ", ";
-                } else {
-                  message += person.first_name + " " + person.last_name + ".";
-                }
-              });
-            }
-            getPeople();
-            AuthFactory.messageModalOpen(message);
-          })
-          .catch(function(error) {
-            message = 'An error occured while applying your group. Please refresh the page and try again.';
-            AuthFactory.messageModalOpen(message);
-          });
-      } else {
-        message = "This group already exists on selected person(s).";
-        AuthFactory.messageModalOpen(message);
-      }
     };
 
     vm.showAll = function() {
