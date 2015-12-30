1d1a75a35f3daff2bb4a43a831c69b2a6c8b8b8d
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 15:59:37 -0800

    working on share mailer

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -3,6 +3,12 @@ class ShareMailer < ApplicationMailer
   def new_user(sending_user, receiving_user)
     @sending_user = sending_user
     @receiving_user = receiving_user
+    @new_auth_header = @receiving_user.create_new_auth_token
+    puts 'about'
+    puts 'to'
+    puts 'puts'
+    puts 'auth header'
+    puts @new_auth_header
     mail(to: @receiving_user.email, subject: "#{@sending_user.first_name} #{@sending_user.last_name} has invited you to Know Your Circle!" )
   end
 
