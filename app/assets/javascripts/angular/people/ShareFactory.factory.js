af0f84286fb6803a57a0dd3ea3e6383b347e82e6
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 09:07:45 -0800

    created ShareFactory

---
+++ app/assets/javascripts/angular/people/ShareFactory.factory.js
@@ -0,0 +1,36 @@
+(function() {
+  angular.module('knowYourCircle.people')
+    .factory('ShareFactory', ShareFactory);
+
+  ShareFactory.$inject = ['$uibModal'];
+
+  function ShareFactory($uibModal) {
+    var shareFactory = {};
+
+    shareFactory.shareSelectedModalOpen = function() {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: '/partials/people/_shareSelected_modal.html',
+        controller: 'shareSelectedController as modal',
+        windowClass: "modal fade"
+      });
+    };
+
+    shareFactory.shareGroupsModalOpen = function(groups) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: '/partials/people/_shareGroups_modal.html',
+        controller: 'shareGroupsController as modal',
+        windowClass: "modal fade",
+        size: 'sm',
+        resolve: {
+          groups: function() {
+            return groups;
+          }
+        }
+      });
+    };
+
+    return shareFactory;
+  }
+})();
