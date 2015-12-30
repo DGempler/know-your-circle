4f10c9c34025fb64998b4d46818047f5461c4090
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 11:07:10 -0800

    got share create action to work for groups, except for images

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -1,37 +1,52 @@
-class PeopleController < ApplicationController
+class ShareController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.new share_params[:email]
+    other_user = User.where(email: share_params[:email])
+
+
+    unless other_user.present?
+      other_user_existed = false
+      other_user = User.new(email: share_params[:email])
+    else
+      other_user_existed = true
+      other_user = other_user[0]
+    end
 
     people = []
 
     share_params[:group_ids].each do |id|
-      group = current_user.groups.includes(:people).find(id)
+      group = current_user.groups.find(id)
       group.people.each do |person|
         people << person if !people.include? person
       end
-
-    puts 'about'
-    puts 'to'
-    puts 'puts'
-    puts 'people'
-    puts people
-    puts user
-
     end
 
-    # if @person.save
-    #   render json: @person, status: :created
-    # else
-    #   render json: @person.errors, status: :unprocessable_entity
-    # end
+
+    people.each do |person|
+      other_user.people << person.dup
+    end
+
+    unless other_user_existed
+      if other_user.save!(validate: false)
+        render json: other_user, status: :created
+      else
+        render json: other_user.errors, status: :unprocessable_entity
+      end
+    else
+      if other_user.save
+        render json: other_user, status: :ok
+      else
+        render json: other_user.errors, status: :unprocessable_entity
+      end
+    end
+
   end
 
   private
   def share_params
-    params[:user][:group_ids] ||= []
-    params.require(:user).permit(:email, group_ids: [])
+    params[:share][:group_ids] ||= []
+    params.require(:share).permit(:email, group_ids: [])
   end
 
 end
