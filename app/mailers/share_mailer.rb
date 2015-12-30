a0868b557df91e54b8daf2ed2d84dd4909e80145
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 12:45:16 -0800

    removed extra space

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -4,7 +4,6 @@ class ShareMailer < ApplicationMailer
     @sending_user = sending_user
     @receiving_user = receiving_user
     mail(to: @receiving_user.email, subject: "#{@sending_user.first_name} #{@sending_user.last_name} has invited you to Know Your Circle!" )
-
   end
 
   def existing_user(sending_user, receiving_user)
