8561fa96f381f52f1092b4d2b9fe269e96b0b213
Dmitry Gempler <DGempler@gmail.com>
2015-12-29 16:02:41 -0800

    changed to vm syntax

--- app/assets/javascripts/angular/people/person.show.controller.js
+++ app/assets/javascripts/angular/people/person.show.controller.js
@@ -2,38 +2,39 @@
   angular.module('knowYourCircle.people')
     .controller('personShowController', personShowController);
 
-  personShowController.$inject = ['$scope', '$routeParams', '$location', 'PersonFactory', 'UserFactory', 'AuthFactory'];
+  personShowController.$inject = ['$routeParams', '$location', 'PersonFactory', 'UserFactory', 'AuthFactory'];
 
-  function personShowController($scope, $routeParams, $location, PersonFactory, UserFactory, AuthFactory) {
-      PersonFactory.getPerson($routeParams.id)
-        .then(function(person) {
-          $scope.person = person;
-          if ($scope.person.dob === 'null' || $scope.person.dob === null) {
-            $scope.person.dob = "";
-          } else {
-            $scope.person.dob = moment($scope.person.dob).format("MMM Do YYYY");
-          }
-        })
-        .catch(function(error) {
-          var message = 'An error occured while loading your person. Please refresh the page to try again.';
-          AuthFactory.messageModalOpen(message);
-        });
+  function personShowController($routeParams, $location, PersonFactory, UserFactory, AuthFactory) {
+    var vm = this;
+    PersonFactory.getPerson($routeParams.id)
+      .then(function(person) {
+        vm.person = person;
+        if (vm.person.dob === 'null' || vm.person.dob === null) {
+          vm.person.dob = "";
+        } else {
+          vm.person.dob = moment(vm.person.dob).format("MMM Do YYYY");
+        }
+      })
+      .catch(function(error) {
+        var message = 'An error occured while loading your person. Please refresh the page to try again.';
+        AuthFactory.messageModalOpen(message);
+      });
 
-    $scope.deletePerson = function() {
-      var message = "Are you sure?";
-      UserFactory.confirmMessageModalOpen(message)
-        .then(function() {
-          PersonFactory.deletePerson($routeParams.id)
-            .then(function(person) {
-              if (person) {
-                $location.path('/people/index');
-              }
-            })
-            .catch(function(error) {
-              var message = 'An error occured while deleting your person. Please refresh the page and try again.';
-              AuthFactory.messageModalOpen(message);
-            });
-        });
+  vm.deletePerson = function() {
+    var message = "Are you sure?";
+    UserFactory.confirmMessageModalOpen(message)
+      .then(function() {
+        PersonFactory.deletePerson($routeParams.id)
+          .then(function(person) {
+            if (person) {
+              $location.path('/people/index');
+            }
+          })
+          .catch(function(error) {
+            var message = 'An error occured while deleting your person. Please refresh the page and try again.';
+            AuthFactory.messageModalOpen(message);
+          });
+      });
     };
   }
 })();
