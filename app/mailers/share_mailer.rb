d3fe36734c6f8b5359d8d96fac513d286af7669f
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 11:58:49 -0800

    created prelim new and existing user methods

--- app/mailers/share_mailer.rb
+++ app/mailers/share_mailer.rb
@@ -1,2 +1,19 @@
 class ShareMailer < ApplicationMailer
+
+  def new_user(sending_user, receiving_user)
+    @sending_user = sending_user
+    @receiving_user = receiving_user
+    # create env variable for dev/production
+    @url = 'http://localhost:3000/#/sign_up'
+    mail(to: @receiving_user.email, subject: "#{@sending_user.first_name} #{@sending_user.last_name} has invited you to Know Your Circle!" )
+
+  end
+
+  def existing_user(sending_user, receiving_user)
+    @sending_user = sending_user
+    @receiving_user = receiving_user
+    # create env variable for dev/production
+    @url = 'http://localhost:3000/#/log_in'
+    mail(to: @receiving_user.email, subject: "#You've been sent some people!" )
+  end
 end
