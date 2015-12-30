aa49f391f7ec3b17ecfab4c2ae390297b9a1aa49
Dmitry Gempler <DGempler@gmail.com>
2015-12-29 16:06:47 -0800

    created demo show controller

---
+++ app/assets/javascripts/angular/people/person.demo.show.controller.js
@@ -0,0 +1,24 @@
+(function() {
+  angular.module('knowYourCircle.people')
+    .controller('personDemoShowController', personDemoShowController);
+
+  personDemoShowController.$inject = ['$routeParams', 'DemoFactory', 'AuthFactory'];
+
+  function personDemoShowController($routeParams, DemoFactory, AuthFactory) {
+    var vm = this;
+
+    DemoFactory.getGuestUserPerson($routeParams.id)
+      .then(function(person) {
+        vm.person = person;
+        if (vm.person.dob === 'null' || vm.person.dob === null) {
+          vm.person.dob = "";
+        } else {
+          vm.person.dob = moment(vm.person.dob).format("MMM Do YYYY");
+        }
+      })
+      .catch(function(error) {
+        var message = 'An error occured while loading your person. Please refresh the page to try again.';
+        AuthFactory.messageModalOpen(message);
+      });
+  }
+})();
