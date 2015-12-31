c343163d077450f9ecb6b1c2f6880733f48d7d29
Dmitry Gempler <DGempler@gmail.com>
2015-12-30 16:02:48 -0800

    added updateUser if email exists to auth signup controller

--- app/assets/javascripts/angular/auth/auth.signup.controller.js
+++ app/assets/javascripts/angular/auth/auth.signup.controller.js
@@ -14,7 +14,7 @@
       }
 
       vm.signUp = function(isValid) {
-        if (isValid) {
+        if (isValid && !vm.alreadyHasEmail) {
           AuthFactory.signUp(vm.user, $uibModalInstance)
             .catch(function(failure) {
               vm.failureToggle = !vm.failureToggle;
@@ -26,6 +26,9 @@
                 vm.refresh = true;
               }
             });
+        }
+        if (isValid && vm.alreadyHasEmail) {
+          AuthFactory.updateUser(vm.user);
         }
       };
 
