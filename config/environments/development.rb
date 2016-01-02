62f42f4277ce89e2bbd7c136354fd1e298c33b6b
Dmitry Gempler <DGempler@gmail.com>
2016-01-02 13:18:59 -0800

    added mandrill back as email provider

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['MANDRILL_USERNAME'],
+    :password  => ENV['MANDRILL_PASSWORD'] # SMTP password is any valid API key
+  }
 
 
 end
