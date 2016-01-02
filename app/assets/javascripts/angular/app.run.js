6c61b75a8c0e6228c28c5994b2cbd736eff56c40
Dmitry Gempler <DGempler@gmail.com>
2016-01-02 12:52:57 -0800

    added error handling back in to app.run

--- app/assets/javascripts/angular/app.run.js
+++ app/assets/javascripts/angular/app.run.js
@@ -1,16 +1,11 @@
-/*angular.module('knowYourCircle')
+angular.module('knowYourCircle')
   .run(run);
 
   run.$inject = ['$rootScope', 'AuthFactory', 'UserFactory'];
 
   function run($rootScope, AuthFactory, UserFactory) {
-    $rootScope.$on("auth:email-confirmation-success", function(ev, data) {
-      var name = data.first_name + " " + data.last_name;
-      UserFactory.welcomeModalOpen(name);
-    });
-
     $rootScope.$on("auth:email-confirmation-error", function(ev, data) {
       var message = "There was an error. Please try again.";
       AuthFactory.messageModalOpen(message);
     });
-  }*/
+  }
