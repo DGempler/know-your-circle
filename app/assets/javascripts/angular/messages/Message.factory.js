eefcf58bfbd90962e1023e0968808ca04afa0798
Dmitry Gempler <DGempler@gmail.com>
2016-01-08 18:21:39 -0800

    added Message factory

---
+++ app/assets/javascripts/angular/messages/Message.factory.js
@@ -0,0 +1,32 @@
+(function() {
+  angular.module('knowYourCircle.auth')
+    .factory('Message', Message);
+
+    Message.$inject = ['$uibModal'];
+
+    function Message($uibModal) {
+      var factory = {};
+
+      factory.open = function(message, email) {
+        var modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: '/partials/auth/_message_modal.html',
+          controller: 'messageController as message',
+          windowClass: "modal fade",
+          resolve: {
+            message: function() {
+              return message;
+            },
+            email: function() {
+              return email;
+            }
+          }
+        });
+      };
+
+      return factory;
+    }
+
+
+
+})();
