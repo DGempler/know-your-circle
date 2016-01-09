f9f69b3afc6a7d9562dcd5075f39cf1f2c9f05f3
Dmitry Gempler <DGempler@gmail.com>
2016-01-08 18:28:38 -0800

    changed to new Message module for demo show controller

--- app/assets/javascripts/angular/people/demo.show.controller.js
+++ app/assets/javascripts/angular/people/demo.show.controller.js
@@ -2,9 +2,9 @@
   angular.module('knowYourCircle.people')
     .controller('peopleDemoShowController', peopleDemoShowController);
 
-  peopleDemoShowController.$inject = ['$routeParams', 'DemoFactory', 'AuthFactory'];
+  peopleDemoShowController.$inject = ['$routeParams', 'DemoFactory', 'Message'];
 
-  function peopleDemoShowController($routeParams, DemoFactory, AuthFactory) {
+  function peopleDemoShowController($routeParams, DemoFactory, Message) {
     var vm = this;
     vm.demoMode = true;
     vm.busy = true;
@@ -20,7 +20,7 @@
       })
       .catch(function(error) {
         var message = 'An error occured while loading your person. Please refresh the page to try again.';
-        AuthFactory.messageModalOpen(message);
+        Message.open(message);
       })
       .finally(function() {
         vm.busy = false;
