c81094f51ccec6962b06f2827151838cea2291ea
Dmitry Gempler <DGempler@gmail.com>
2016-01-08 16:03:54 -0800

    changed vm.people.people to vm.people

--- app/assets/javascripts/angular/games/games.index.controller.js
+++ app/assets/javascripts/angular/games/games.index.controller.js
@@ -12,7 +12,7 @@
     if ($routeParams.demo === 'true') {
       DemoFactory.getGuestUserPeople()
         .then(function(people) {
-          vm.people.people = people;
+          vm.people = people;
           getDemoGroups();
         })
         .catch(function(people) {
@@ -23,7 +23,7 @@
     } else {
       PersonFactory.getPeople()
         .then(function(people) {
-          vm.people.people = people;
+          vm.people = people;
           getGroups();
         })
         .catch(function(people) {
@@ -66,7 +66,7 @@
         controller: 'gamesFlashcardsController as game',
         resolve: {
           people: function() {
-            return vm.people.people;
+            return vm.people;
           },
           groups: function() {
             return vm.groups;
