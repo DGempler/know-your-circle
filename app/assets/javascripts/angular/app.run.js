07a80783e3ac169919e6d6ed832b28d4c273e111
Dmitry Gempler <DGempler@gmail.com>
2016-01-08 18:24:34 -0800

    changed to new Message module for app.run

--- app/assets/javascripts/angular/app.run.js
+++ app/assets/javascripts/angular/app.run.js
@@ -1,11 +1,11 @@
 angular.module('knowYourCircle')
   .run(run);
 
-  run.$inject = ['$rootScope', 'AuthFactory', 'UserFactory'];
+  run.$inject = ['$rootScope', 'Message', 'UserFactory'];
 
-  function run($rootScope, AuthFactory, UserFactory) {
+  function run($rootScope, Message, UserFactory) {
     $rootScope.$on("auth:email-confirmation-error", function(ev, data) {
-      var message = "There was an error. Please try again.";
-      AuthFactory.messageModalOpen(message);
+      var message = "There was an error. Please refresh and try again.";
+      Message.messageModalOpen(message);
     });
   }
