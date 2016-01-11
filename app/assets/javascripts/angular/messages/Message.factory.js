f535747643903215bfbb648adc98d277607b717c
Dmitry Gempler <DGempler@gmail.com>
2016-01-11 07:48:37 -0800

    moved confirmMessage modal open function from UserFactory to Message factory

--- app/assets/javascripts/angular/messages/Message.factory.js
+++ app/assets/javascripts/angular/messages/Message.factory.js
@@ -24,6 +24,30 @@
         });
       };
 
+      factory.openConfirm = function(message) {
+        var deferred = $q.defer();
+        var modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: '/partials/users/_confirmMessage_modal.html',
+          controller: 'confirmMessageController as message',
+          windowClass: "modal fade",
+          resolve: {
+            message: function() {
+              return message;
+            }
+          }
+        });
+
+        modalInstance.result.then(function() {
+          deferred.resolve();
+        }, function() {
+          deferred.reject();
+        });
+
+        return deferred.promise;
+      };
+
+
       return factory;
     }
 
--- app/assets/javascripts/angular/users/UserFactory.factory.js
+++ app/assets/javascripts/angular/users/UserFactory.factory.js
@@ -7,29 +7,6 @@
     function UserFactory($uibModal, $q) {
       var factory = {};
 
-      factory.confirmMessageModalOpen = function(message) {
-        var deferred = $q.defer();
-        var modalInstance = $uibModal.open({
-          animation: true,
-          templateUrl: '/partials/users/_confirmMessage_modal.html',
-          controller: 'confirmMessageController as message',
-          windowClass: "modal fade",
-          resolve: {
-            message: function() {
-              return message;
-            }
-          }
-        });
-
-        modalInstance.result.then(function() {
-          deferred.resolve();
-        }, function() {
-          deferred.reject();
-        });
-
-        return deferred.promise;
-      };
-
       factory.welcomeModalOpen = function(name) {
         var modalInstance = $uibModal.open({
           animation: true,
