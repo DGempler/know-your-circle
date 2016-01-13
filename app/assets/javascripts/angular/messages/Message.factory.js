24e92536730fbbc06cdb7926e53dedd38a22a2dd
Dmitry Gempler <DGempler@gmail.com>
2016-01-12 18:45:18 -0800

    refactored Message factory

--- app/assets/javascripts/angular/messages/Message.factory.js
+++ app/assets/javascripts/angular/messages/Message.factory.js
@@ -7,36 +7,40 @@
     function Message($uibModal, $q) {
       var factory = {};
 
-      factory.open = function(message, email) {
-        var modalInstance = $uibModal.open({
+      function createModalInstance(fileName, controllerName, resolveObject) {
+        return $uibModal.open({
           animation: true,
-          templateUrl: '/partials/messages/_message_modal.html',
-          controller: 'messageController as message',
+          templateUrl: '/partials/messages/' + fileName + '.html',
+          controller: controllerName + ' as message',
           windowClass: "modal fade",
-          resolve: {
-            message: function() {
-              return message;
-            },
-            email: function() {
-              return email;
-            }
+          resolve: resolveObject
+          });
+      }
+
+      factory.open = function(message, email) {
+        var fileName = '_message_modal';
+        var controllerName = 'messageController';
+        var resolve = {
+          message: function() {
+            return message;
+          },
+          email: function() {
+            return email;
           }
-        });
+        };
+        createModalInstance(fileName, controllerName, resolve);
       };
 
       factory.openConfirm = function(message) {
         var deferred = $q.defer();
-        var modalInstance = $uibModal.open({
-          animation: true,
-          templateUrl: '/partials/messages/_confirmMessage_modal.html',
-          controller: 'confirmMessageController as message',
-          windowClass: "modal fade",
-          resolve: {
-            message: function() {
-              return message;
-            }
+        var fileName = '_confirmMessage_modal';
+        var controllerName = 'confirmMessageController';
+        var resolve = {
+          message: function() {
+            return message;
           }
-        });
+        };
+        var modalInstance = createModalInstance(fileName, controllerName, resolve);
         modalInstance.result.then(function() {
           deferred.resolve();
         }, function() {
@@ -46,22 +50,18 @@
       };
 
       factory.openWelcome = function(name) {
-        var modalInstance = $uibModal.open({
-          animation: true,
-          templateUrl: '/partials/messages/_welcome_modal.html',
-          controller: 'welcomeController as message',
-          windowClass: "modal fade",
-          resolve: {
-            name: function() {
-              return name;
-            }
+        var fileName = '_welcome_modal';
+        var controllerName = 'welcomeController';
+        var resolve = {
+          name: function() {
+            return name;
           }
-        });
+        };
+
+        createModalInstance(fileName, controllerName, resolve);
       };
 
       return factory;
     }
-
-
 
 })();
