1ed3b1cf08741fc25df6b64df30461c84e946436
Dmitry Gempler <DGempler@gmail.com>
2016-01-14 07:13:56 -0800

    refactored changePasswordController

--- app/assets/javascripts/angular/users/users.changePassword.controller.js
+++ app/assets/javascripts/angular/users/users.changePassword.controller.js
@@ -7,26 +7,38 @@
     function changePasswordController($uibModalInstance, AuthFactory, Message) {
       var vm = this;
 
+      function submitChangePasswordSuccess() {
+        vm.closeModal();
+        var message = "Your password has been successfully updated.";
+        Message.open(message);
+      }
+
+      function submitChangePasswordError(failure) {
+        vm.failureToggle = !vm.failureToggle;
+        vm.error = failure.data.errors.full_messages[0];
+      }
+
+      function submitChangePassword() {
+        vm.busy = true;
+        AuthFactory.submitChangePassword(vm.user)
+          .then(function() {
+            submitChangePasswordSuccess();
+          })
+          .catch(function(failure) {
+            submitChangePasswordError(failure);
+          })
+          .finally(function() {
+            vm.busy = false;
+          });
+      }
+
       vm.closeModal = function() {
         $uibModalInstance.close();
       };
 
       vm.submitChangePassword = function(isValid) {
         if (isValid) {
-          vm.busy = true;
-          AuthFactory.submitChangePassword(vm.user)
-            .then(function() {
-              $uibModalInstance.close();
-              var message = "Your password has been successfully updated.";
-              Message.open(message);
-            })
-            .catch(function(failure) {
-              vm.failureToggle = !vm.failureToggle;
-              vm.error = failure.data.errors.full_messages[0];
-            })
-            .finally(function() {
-              vm.busy = false;
-            });
+          submitChangePassword();
         }
       };
 
